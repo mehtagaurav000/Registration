336448613739c7d851438da6165cb46a3c596b53
mehta.gaurav000 <mehta.gaurav000@gmail.com>
2019-04-15 18:33:08 +0530

    Excel code

--- src/main/java/Hotel/Registration/Generic/ExcelUtils.java
+++ src/main/java/Hotel/Registration/Generic/ExcelUtils.java
@@ -5,6 +5,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -35,6 +38,56 @@ public class ExcelUtils {
 		wBook=new XSSFWorkbook(fis);
 		return wBook;
 	} 
+	
+	public int getRowNumber(Sheet sheetObj,String testCaseId)
+	{
+		int lastRowNum=sheetObj.getLastRowNum();
+		int finalRowNum=0;
+		for(int i=0;i<=lastRowNum;i++)
+		{
+			Row rowObj=sheetObj.getRow(i);
+			Cell cellObj=rowObj.getCell(0);
+			String data=cellObj.getStringCellValue();
+			if(data.equalsIgnoreCase(testCaseId))
+			{
+				finalRowNum=i;
+				break;
+			}
+		}
+		
+		return finalRowNum;
+		
+	}
+	
+	public HashMap<String,String> getData(String filePath,String sheetName,String testCaseId) throws IOException
+	{
+		Workbook workbookObj=getWorkbook(filePath);
+		
+		HM=new HashMap<String, String>();
+		
+		Sheet sheetObj=workbookObj.getSheet(sheetName);
+		
+		int rowNum=getRowNumber(sheetObj, testCaseId);
+		
+		Row rowObj=sheetObj.getRow(rowNum);
+		
+		int cellNum=rowObj.getLastCellNum()-1;
+		
+		for(int i=2;i<=cellNum;i+=2)
+		{
+		Cell cellobj=rowObj.getCell(i,Row.CREATE_NULL_AS_BLANK);
+			String key=cellobj.getStringCellValue();
+			
+			Cell cellobj1=rowObj.getCell(i,Row.CREATE_NULL_AS_BLANK);
+			String value=cellobj1.getStringCellValue();
+			HM.put(key, value);
+		}
+		
+		
+		return HM;
+		
+	}
+	
 	
 	
 
